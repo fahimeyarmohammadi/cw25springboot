34ad89549412d91f4475b1848a52ec1c65b08239
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2023-01-13 14:50:32 +0330

    Initial commit

---
+++ src/main/java/com/maktab/cw25springboot/Cw25springbootApplication.java
@@ -0,0 +1,13 @@
+package com.maktab.cw25springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw25springbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw25springbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maktab/cw25springboot/Cw25springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maktab.cw25springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw25springbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
